1d5b2b10309736f651b8d246be156c6a8c5b3188
okuniNW <118362040+okuniNW@users.noreply.github.com>
2026-09-04 14:00:28 +0700

    Increase padding in AppPage component

--- src/pages/AppPage.jsx
+++ src/pages/AppPage.jsx
@@ -50,7 +50,7 @@ export default function AppPage() {
         <div style={{
           maxWidth: '480px',
           margin: '0 auto',
-          padding: '16px 16px 32px',
+          padding: '16px 16px 80px',
         }}>
           {renderTab()}
         </div>
